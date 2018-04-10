424fda77453712e64c187c30c002192a448ddc20
i403606 <i403606@git.fhict.nl>
2018-04-10 10:13:53 +0200

    Layout with add/remove functionality

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import Grid2 from './components/Grid2.jsx';
+
+
+class App extends Component {
+  render() {
+    return (
+			<Grid2/>
+    );
+  }
+}
+
+export default App;
